daf7bb9be5bc497dc48009dca3258e36d3b08cfe
Explore-728 <1828654140@qq.com>
2026-09-14 12:26:44 +0800

    v0

--- 7-3.c
+++ 7-3.c
@@ -3,7 +3,16 @@
 
 int main()
 {
-    printf("Hello,World");
+    int i,a,b,c,d;
+
+    scanf("%d",&i);
+    a = i/100;
+    b = i/10%10;
+    c = i%100;
+    d = c*100 + b*10 + a;
+
+    printf("%d",d);
+
     system("pause");
     
     return 0;
