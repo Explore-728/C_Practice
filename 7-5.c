7aa629fdc6301e1d82610aca64516379e0e18c2f
Explore-728 <1828654140@qq.com>
2026-09-17 13:51:11 +0800

    v0

--- 7-5.c
+++ 7-5.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
     printf("------------------------------------\n");
-    printf("Province      Area(km2)    Pop.(10K)\n");
+    printf("Province      Area(km2)   Pop.(10K)\n");
     printf("------------------------------------\n");
     printf("Anhui         139600.00   6461.00\n");
     printf("Beijing        16410.54   1180.70\n");
@@ -12,8 +11,6 @@ int main()
     printf("Shanghai        6340.50   1360.26\n");
     printf("Zhejiang      101800.00   4894.00\n");
     printf("------------------------------------\n");
-
-    system("pause");
 
     return 0;
 
