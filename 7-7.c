2c5cad96ddd49e0ba8f8dd0dfe982a558a716ff4
Explore-728 <1828654140@qq.com>
2026-09-19 15:17:47 +0800

    v2 修改了小时的保留

--- 7-7.c
+++ 7-7.c
@@ -7,9 +7,9 @@ int main()
     scanf("%d:%d",&a,&b);
 
     if( a < 12){
-        printf("%02d:%02d AM",a,b);
+        printf("%d:%02d AM",a,b);
     }else{
-        printf("%02d:%02d PM",a - 12,b);
+        printf("%d:%02d PM",a - 12,b);
     }
     system("pause");
     return 0;
